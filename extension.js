4aadf29e2652d914aaeb744f1b50e85c7c047f04
Per Thomas Lundal <pt@lundal.io>
2022-01-18 20:45:34 +0100

    Resize window again because of a bug in GTK with Mutter 41.3

--- extension.js
+++ extension.js
@@ -221,6 +221,10 @@ class Extension {
             window.unmaximize(Meta.MaximizeFlags.HORIZONTAL | Meta.MaximizeFlags.VERTICAL);
         }
         window.move_resize_frame(true, area.x, area.y, area.width, area.height);
+        // Move the window again because of a bug in GTK with Mutter 41.3
+        // See https://gitlab.gnome.org/GNOME/mutter/-/issues/2091 for details
+        // This resolves the issue for many but not all applications
+        window.move_resize_frame(true, area.x, area.y, area.width, area.height);
         // In some cases move_resize_frame() will resize but not move the window, so we need to move it again.
         // This usually happens when the window's minimum size is larger than the selected area.
         window.move_frame(true, area.x, area.y);
